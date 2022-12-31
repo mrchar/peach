831a87f5aa848159a000c7404144e2d0c98e2730
mrchar <mrchar@qq.com>
2022-12-31 09:40:13 +0800

    解决跨域的问题

---
+++ peach-authorization-server/src/main/java/com/github/mrchar/peach/authorization/config/WebSecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.github.mrchar.peach.authorization.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class WebSecurityConfiguration {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173/", "http://127.0.0.1:5173/"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(authorize -> {
+            authorize.requestMatchers(HttpMethod.POST, "/api/register").permitAll();
+            authorize.anyRequest().authenticated();
+        });
+        httpSecurity.formLogin(withDefaults());
+        httpSecurity.cors(withDefaults());
+        httpSecurity.csrf().disable();
+        return httpSecurity.build();
+    }
+}
+
